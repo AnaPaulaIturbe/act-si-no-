e1fecbadb0d7f12d610af0cc67a00e5e995c6229
AnaPaulaIturbe <112582690+AnaPaulaIturbe@users.noreply.github.com>
2022-10-05 12:35:30 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,48 @@
 let num_1 = 0
 let num_2 = 0
 let num_3 = 0
+let mayor = 0
+let medio = 0
+let menor = 0
 input.onButtonPressed(Button.A, function () {
-    if (num_1 == 0 && num_2 == 0) {
-        basic.showIcon(IconNames.No)
-    } else {
-    	
+    if (num_1 > num_2 && num_1 > num_3) {
+        mayor = num_1
+        if (num_2 > num_3) {
+            medio = num_2
+            menor = num_3
+        } else {
+            medio = num_3
+            menor = num_2
+        }
+        basic.showNumber(mayor)
+        basic.showNumber(medio)
+        basic.showNumber(menor)
+    }
+    if (num_2 > num_1 && num_2 > num_3) {
+        mayor = num_2
+        if (num_1 > num_3) {
+            medio = num_1
+            menor = num_3
+        } else {
+            medio = num_3
+            menor = num_1
+        }
+        basic.showNumber(mayor)
+        basic.showNumber(medio)
+        basic.showNumber(menor)
+    }
+    if (num_3 > num_1 && num_3 > num_2) {
+        mayor = num_3
+        if (num_1 > num_2) {
+            medio = num_1
+            menor = num_2
+        } else {
+            medio = num_2
+            menor = num_1
+        }
+        basic.showNumber(mayor)
+        basic.showNumber(medio)
+        basic.showNumber(menor)
     }
 })
 input.onGesture(Gesture.Shake, function () {
